6a95e0096038a6d71086fd0e1ccb16de6bc1d65f
KRNelson <kevin92620@yahoo.com>
2015-04-26 18:29:30 -0700

    Added description to git.sh

--- git.sh
+++ git.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Kevin Nelson
+# 4/26/15
+
+# List files that you want to commit as arguments to the script. 
+# ie. ./git.sh example.html.erb
+#
+# After executing the script, you will be asked to supply a
+# commit message, along with login details for github. 
+
 git add $@ # List of files entered as command line arguments
 # Prompt user for a commit message!
 IFS= read -r -p "Enter a commit message: " message

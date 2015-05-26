4cdb7b579afe2a4bbaf9d64923e61bef2de69b69
Tony Miller <amille49@calpoly.edu>
2015-05-25 21:05:28 -0700

    before ajaxification

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Pier::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'videoviewer#video'
+  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
